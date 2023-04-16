aa489db0d5b237634f0adc66eb07561a827fa497
BeluG <belengimenez0501@gmail.com>
2023-04-15 21:59:52 -0300

    inserta Mensaje aquí

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Mundo Bajito
+
+Curso de Javascript
+Primera Entrega Proyecto Final
+## Authors
+
+- Gimenez, Maria Belen (Alumna en Coderhouse)
+
+
+## Documentation
+
+El presente contiene un archivo HTML, con su archivo js, el cual contiene un ciclo For y un If, para demostrar lo aprendido hasta ahora en el curso.
+
+## Feedback
+
+Este proyecto es solamente de uso para correción de los tutores, su estructura no puede ser o debiera ser tomada necesariamente como propuesta para un proyecto real.
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-let localidad = prompt ("Si sos de San Justo, ingresa 1. Si sos de Ramos Mejia, ingresa 2. Si sos de Moron, ingresa 3. Si no perteneces a ninguna de estas zonas, ingresa 0.");
+let localidad = prompt("Si sos de San Justo, ingresa 1. Si sos de Ramos Mejia, ingresa 2. Si sos de Moron, ingresa 3. Si no perteneces a ninguna de estas zonas, ingresa 0.");
 const localidadSANJUSTO = 1
 const localidadRAMOSMEJIA = 2
 const localidadMORON = 3
@@ -8,18 +8,52 @@ if (localidad == localidadSANJUSTO) {
     alert("Buenísimo! Los envíos son sin costo a la localidad de San Justo :D");
 } else if (localidad == localidadRAMOSMEJIA) {
     alert("Los envíos a la localidad de Ramos Mejia tienen un valor adicional de $150.");
-}else if (localidad == localidadMORON) {
+} else if (localidad == localidadMORON) {
     alert("Los envíos a la localidad de Moron tienen un valor adicional de $250.");
-} else if (localidad == localidadLEJANA){
+} else if (localidad == localidadLEJANA) {
     alert("Lástima, por el momento no realizamos envíos por tu zona.");
-} 
+}
 else {
     alert("La respuesta ingresada no es válida.")
 }
 
-let numeroEnvios = 4;
+let numeroEnvios = 3;
 for (let envio = 1; envio <= numeroEnvios; envio++) {
-  let nombre = prompt("¿Cómo se llama la persona que realizó el pedido " + envio + "?");
-  alert("Realizó el pedido " + nombre + ", tiene asignado el envío número " + envio);
-  console.log("Realizó el pedido " + nombre + ", tiene asignado el envío número " + envio);
+    let nombre = prompt("¿Cómo se llama la persona que realizó el pedido " + envio + "?");
+    alert("Realizó el pedido " + nombre + ", tiene asignado el envío número " + envio);
+    console.log("Realizó el pedido " + nombre + ", tiene asignado el envío número " + envio);
 }
+
+//Acá empieza lo que sumé para la Pre-Entrega nº2
+
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre.toUpperCase();
+        this.precio = parseFloat(precio);
+    }
+}
+
+let misProductos = [
+    new Producto("Pampers Premium RN x36un", 2220),
+    new Producto("Pampers Confort RN+ x36un", 1850),
+    new Producto("Huggies Flex RN x34un", 1640),
+    new Producto("Huggies Supreme RN x34un", 2310),
+    new Producto("Babysec RN x20un", 1370)
+];
+
+function calcularCostoTotal(productos = []) {
+    let total = 0;
+    for (const unProducto of productos) {
+        total += unProducto.precio;
+    }
+    return total;
+}
+
+let costoTotal = calcularCostoTotal(misProductos);
+
+//Usando la función de orden superior MAP
+let nombres = misProductos.map((unProducto) => unProducto.nombre.join(“, ”));
+alert("Lista de productos que compró el usuario: " + nombres);
+
+
+alert("El usuario deberá abonar un total de $" + costoTotal.toFixed(2) + "por la compra de " + misProductos.length + " productos.");
